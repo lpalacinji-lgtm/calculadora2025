6a9d45526cbf439189c9a5b8fb23c8090fac22ca
lpalacinji-lgtm <lpalacinji@gmail.com>
2025-11-13 08:28:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -94,33 +94,20 @@ import os
 # ======================================
 # ENCABEZADO CON LOGO Y TÍTULO CENTRADO
 # ======================================
-logo_path = os.path.join("logo", "logo1.png")
-
+logo_path = "logo/logo1.png"
 if os.path.exists(logo_path):
     with open(logo_path, "rb") as f:
         logo_base64 = base64.b64encode(f.read()).decode("utf-8")
-
     st.markdown(
         f"""
-        <style>
-        .app-header {{
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            padding: 10px 0;
-        }}
-        .app-header img {{
-            height: 80px;
-        }}
-        </style>
         <div class="app-header">
             <img src="data:image/png;base64,{logo_base64}" alt="Logo">
         </div>
         """,
         unsafe_allow_html=True
-    )
+)
 else:
-    st.title("🖩 Calculadora de Medicamentos 💊")
+    st.title("🖩 calculadora")
 
 # ======================================
 # LAYOUT PRINCIPAL
@@ -248,6 +235,7 @@ st.markdown("""
         <p class="texto">Está destinado exclusivamente para uso institucional y bajo las políticas de privacidad y seguridad de la compañia . Cualquier divulgación, copia o uso no autorizado está estrictamente prohibido.</p>
     </div>
 """, unsafe_allow_html=True)
+
 
 
 
